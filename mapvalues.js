182db838930d4f4ab85159b3a30d79cd070db1f3
Aripander088e2 <121055226+Aripander088e2@users.noreply.github.com>
2023-10-05 08:22:23 -0400

    Update mapvalues.js

--- mapvalues.js
+++ mapvalues.js
@@ -35,7 +35,7 @@ for(var level = 0; level < 50; level++) {
 function createMap(maps, level, lanes, type) {
 	//var extraDifficulty = (((level+1)/2)|0)
 	var theMap = [];
-	theMap[0] = Math.floor(Math.pow(6.6, level+11)); 					//Base Gold
+	theMap[0] = Math.floor(Math.pow(2.6, level+3)); 					//Base Gold
 	theMap[1] = Math.floor(15*Math.pow(1.25, level)*(level*level*.5+1)+level*16);                               //Territory Gain (with Bonus)
 	theMap[2] = Math.floor((150*Math.pow(1.06, level))*(level*2+1));                                 //Enemy Fence Health
 	theMap[3] = Math.floor((1800*Math.pow(1.5, level)*(level+1)));                                 //Enemy Wall Health
